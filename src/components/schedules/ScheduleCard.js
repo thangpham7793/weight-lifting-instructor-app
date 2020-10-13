c343e251a767c3c3e3e160759e9a61cb92d525e2
Thang <thangnus@gmail.com>
2020-10-13 16:47:30 +1300

    Finish adding Schedule Cards

---
+++ src/components/schedules/ScheduleCard.js
@@ -0,0 +1,76 @@
+import React from "react"
+import { makeStyles } from "@material-ui/core/styles"
+
+import {
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CardActions,
+  IconButton,
+  Avatar,
+  Typography,
+} from "@material-ui/core"
+
+import { red } from "@material-ui/core/colors"
+import { Delete, Edit, Share } from "@material-ui/icons"
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    background: "var(--txt-cl)",
+  },
+  media: {},
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+  actionBtnWrapper: {
+    display: "flex",
+    justifyContent: "space-evenly",
+  },
+  scheduleActionBtn: {
+    border: "none",
+    boxShadow: "none",
+    padding: "var(--pd-sm)",
+    width: "max-content",
+  },
+}))
+
+export default function ScheduleCard() {
+  const classes = useStyles()
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        title="October Peaking Cycle"
+        subheader="September 14, 2016"
+      />
+      <CardContent>
+        <Typography variant="body2" component="p">
+          What should be put in here?
+        </Typography>
+      </CardContent>
+      {/* edit/delete/publish maybe edit should take to a new page */}
+      <CardActions disableSpacing className={classes.actionBtnWrapper}>
+        <IconButton className={classes.scheduleActionBtn} aria-label="edit">
+          <Edit />
+        </IconButton>
+        <IconButton className={classes.scheduleActionBtn} aria-label="publish">
+          <Share />
+        </IconButton>
+        <IconButton className={classes.scheduleActionBtn} aria-label="delete">
+          <Delete />
+        </IconButton>
+      </CardActions>
+    </Card>
+  )
+}
--- src/components/schedules/SchedulePanel.js
+++ src/components/schedules/SchedulePanel.js
@@ -1,5 +1,32 @@
 import React from "react"
+import ScheduleCard from "./ScheduleCard"
+import { Grid } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles"
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    justifyContent: "space-around",
+  },
+  item: {
+    margin: "var(--mg-sm)",
+    padding: 0,
+  },
+}))
 
 export function SchedulePanel() {
-  return <div>Schedule Panel</div>
+  const classes = useStyles()
+
+  return (
+    <Grid container className={classes.container}>
+      <Grid item xs={6} md={3} className={classes.item}>
+        <ScheduleCard />
+      </Grid>
+      <Grid item xs={6} md={3} className={classes.item}>
+        <ScheduleCard />
+      </Grid>
+      <Grid item xs={6} md={3} className={classes.item}>
+        <ScheduleCard />
+      </Grid>
+    </Grid>
+  )
 }
