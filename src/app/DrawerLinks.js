aaf1a85644f0f13e333f6c11625ed0d5b9b1f417
Thang <thangnus@gmail.com>
2020-10-21 15:06:37 +1300

    clean up

--- src/app/DrawerLinks.js
+++ src/app/DrawerLinks.js
@@ -8,7 +8,7 @@ import {
   ListItemText,
   ListItemIcon,
 } from "@material-ui/core"
-import { Menu, FitnessCenter } from "@material-ui/icons"
+import { Menu } from "@material-ui/icons"
 import { makeStyles } from "@material-ui/core/styles"
 import { Link } from "react-router-dom"
 
